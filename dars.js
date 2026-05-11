b2b8adb2e4309ce3e3fe7ece58d9a801979dc2fe
Alisher <alishertoxirov1314@gmail.com>
2026-05-11 21:13:33 +0500

    learn class with js

--- dars.js
+++ dars.js
@@ -1,23 +1,41 @@
-class User {
-  planet = "earth";
-  country = "Uzbek";
-  region; //undefined
+class Person {
+  //   planet = "earth";
+  //   country = "Uzbek";
+  //   region; //undefined
 
   constructor(name, age) {
     this.name = name;
     this.age = age;
   }
-
-  logAge() {
-    console.log(this.age);
+  set() {
+    console.log("Ем...");
   }
+  sleep() {
+    console.log("Сплю..");
+  }
+
+  //   logAge() {
+  //     console.log(this.age);
+  //   }
 }
 
-const firstUser = new User("Alisher", 15)
-const secondUser = new User("Misha", 5)
+// const firstUser = new User("Alisher", 15)
+// const secondUser = new User("Misha", 5)
 
-firstUser.logAge()
-secondUser.logAge()
+// firstUser.logAge()
+// secondUser.logAge()
 
+// firstUser.city = 'Urgench'
+// console.log(firstUser.city);
 
+const example = new Person("Alisher", 15);
 
+class Developer extends Person {
+    constructor(name , age , expirence){
+        super(name , age)
+    }
+}
+
+const exampleDeveloper = new Developer("Misha", 5);
+
+exampleDeveloper.code();
